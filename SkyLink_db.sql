cf34e7622763b31ff4ae58c0e7ee0a2bf5bf7b66
aatje-sy <153828359+aatje-sy@users.noreply.github.com>
2024-06-12 12:59:44 +0200

    SQL Inner Join images with flights
    Added some images

--- SkyLink_db.sql
+++ SkyLink_db.sql
@@ -5,16 +5,36 @@ CREATE TABLE flights(
                         id int not NULL AUTO_INCREMENT PRIMARY KEY,
                         flight_name text,
                         discription text,
-                        price DECIMAL(10,2),
-                        imgs VARCHAR(255)
+                        price DECIMAL(10,2)
 );
 
 
-INSERT INTO flights (flight_name, discription, price, imgs) VALUES ('Italian Landscape', 'hvopeahvgowrphbvwovn ovnweovgnw9ov', 299.99, 'italian-land.jpg');
-INSERT INTO flights (flight_name, discription, price, imgs) VALUES ('Thailand Phi Phi Islands', 'hvopeahvgowrphbvwovn ovnweovgnw9ov', 399.99, 'phiphiIslands.jpg');
-INSERT INTO flights (flight_name, discription, price, imgs) VALUES ('Greece Santorini Island', 'hvopeahvgowrphbvwovn ovnweovgnw9ov', 599.99, '');
-INSERT INTO flights (flight_name, discription, price, imgs) VALUES ('Norway Lofoten Islands', 'hvopeahvgowrphbvwovn ovnweovgnw9ov', 599.99, '');
-INSERT INTO flights (flight_name, discription, price, imgs) VALUES ('Japanese Hakone Gate', 'hvopeahvgowrphbvwovn ovnweovgnw9ov', 799.99, '');
+INSERT INTO flights (flight_name, discription, price) VALUES ('Italian Landscape', 'hvopeahvgowrphbvwovn ovnweovgnw9ov', 299.99);
+INSERT INTO flights (flight_name, discription, price) VALUES ('Thailand Phi Phi Islands', 'hvopeahvgowrphbvwovn ovnweovgnw9ov', 399.99);
+INSERT INTO flights (flight_name, discription, price) VALUES ('Greece Santorini Island', 'hvopeahvgowrphbvwovn ovnweovgnw9ov', 599.99);
+INSERT INTO flights (flight_name, discription, price) VALUES ('Norway Lofoten Islands', 'hvopeahvgowrphbvwovn ovnweovgnw9ov', 599.99);
+INSERT INTO flights (flight_name, discription, price) VALUES ('Japanese Hakone Gate', 'hvopeahvgowrphbvwovn ovnweovgnw9ov', 799.99);
+
+
+DROP TABLE IF EXISTS Images;
+CREATE TABLE Images(
+                        id int not NULL AUTO_INCREMENT PRIMARY KEY,
+                        imags varchar(255),
+                        flight_id int(255)
+);
+
+INSERT INTO Images (imags, flight_id) VALUES ('italian-land.jpg', 1);
+INSERT INTO Images (imags, flight_id) VALUES ('SideImg-Italy.jpg', 1);
+INSERT INTO Images (imags, flight_id) VALUES ('vacation-1-italy.jpg', 1);
+INSERT INTO Images (imags, flight_id) VALUES ('hotel-img-vacation1.png', 1);
+INSERT INTO Images (imags, flight_id) VALUES ('phiphiIslands.jpg', 2);
+INSERT INTO Images (imags, flight_id) VALUES ('phiPhi-hotel.jpg', 2);
+INSERT INTO Images (imags, flight_id) VALUES ('phiphiBoat.jpg', 2);
+INSERT INTO Images (imags, flight_id) VALUES ('phiMountain.jpg', 2);
+
+
+
+SELECT * FROM Images INNER JOIN flights on Images.flight_id = flights.id;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE `users` (
