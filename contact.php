633bdd5a73006d46671d8e41e53e465316c3e25d
osama <osama.asmi04@gmail.com>
2024-06-24 16:04:34 +0200

    login fixes

--- contact.php
+++ contact.php
@@ -1,7 +1,6 @@
 <?php
 require_once('connect.php');
 include_once("login-logic.php");
-
 /**
  * @var $pdo ;
  */
--- send-pass-reset.php
+++ send-pass-reset.php
@@ -1,23 +1,23 @@
 <?php
-require_once "connect.php";
-/**
- * @var $pdo ;
- */
-
-$email = $_POST['forgotEmail'];
-
-$token = bin2hex(random_bytes(16));
-
-$token_hash = hash('sha256', $token);
-
-$expiry =date("Y-m-d H:i:s", time() + 60 * 30);
-
-$sql = "UPDATE users SET rest_token_hash = :token_hash,
-                 rest_token_expires_at = :token_expires_at
-             WHERE email = :email";
-$stmt = $pdo->prepare($sql);
-$stmt->bindParam(':email', $email);
-$stmt->bindParam(':token_hash', $token_hash);
-$stmt->bindParam(':token_expires_at', $expiry);
-$stmt->execute();
+//require_once "connect.php";
+///**
+// * @var $pdo ;
+// */
+//
+//$email = $_POST['forgotEmail'];
+//
+//$token = bin2hex(random_bytes(16));
+//
+//$token_hash = hash('sha256', $token);
+//
+//$expiry =date("Y-m-d H:i:s", time() + 60 * 30);
+//
+//$sql = "UPDATE users SET rest_token_hash = :token_hash,
+//                 rest_token_expires_at = :token_expires_at
+//             WHERE email = :email";
+//$stmt = $pdo->prepare($sql);
+//$stmt->bindParam(':email', $email);
+//$stmt->bindParam(':token_hash', $token_hash);
+//$stmt->bindParam(':token_expires_at', $expiry);
+//$stmt->execute();
 
